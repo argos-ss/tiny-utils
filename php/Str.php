363cb4dee2e819cd525ab85d6a72d9357c1aec0e
ArgosBot <argosbotss@gmail.com>
2026-06-08 17:17:52 +0000

    chore: daily utility update [php] — 2026-06-08 17:17

--- php/Str.php
+++ php/Str.php
@@ -30,3 +30,7 @@ final class Str {
     {
         return str_contains($haystack, $needle);
     }
+    public static function contains(string $haystack, string $needle): bool
+    {
+        return str_contains($haystack, $needle);
+    }
